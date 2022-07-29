21e5841a3ee9b9cea6f9e1e076f7cd63c93383d4
CGJI58 <leemsk22@naver.com>
2022-07-29 19:17:53 +0900

    challenge #4.0~#4.1

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 type Words = {
-  [key: number]: string;
+  [key: string]: string;
 };
 
+class Word {
+  constructor(public term: string, public def: string) {}
+}
 class Dict {
   private words: Words;
   constructor() {
@@ -10,20 +13,41 @@ class Dict {
   add(word: Word) {
     if (this.words[word.term] === undefined) {
       this.words[word.term] = word.def;
+      console.log(`Word '${word.term}' has been added.`);
+    } else {
+      console.log(`Word '${word.term}' is already in the dictionary.`);
     }
   }
-  def(term: string) {
-    return this.words[term];
+  getDef(term: string) {
+    if (this.words[term] === undefined) {
+      console.log(`Cannot find word: ${term}`);
+      return;
+    } else {
+      return this.words[term];
+    }
   }
-}
-
-class Word {
-  constructor(public term: string, public def: string) {}
+  del(term: string) {
+    if (Object.keys(this.words).includes(term)) {
+      delete this.words[term];
+      console.log(`${term} has been deleted.`);
+    } else {
+      console.log(`Cannot find word: ${term}`);
+    }
+  }
 }
 
 const kimchi = new Word("kimchi", "한국의 음식");
-
 const dict = new Dict();
 
-dict.add(kimchi);
-dict.def("kimchi");
+// test add / del
+// dict.add(kimchi);
+// dict.add(kimchi);
+// dict.del("kimchi");
+// dict.del("kimchi");
+
+// test getDef
+// const mydef = dict.getDef("kimchi");
+// console.log(mydef);
+
+//내일 update, showAll, count 구현할 것.
+//https://www.typescriptlang.org/play?#code/C4TwDgpgBA6g9gJwCYGcoF4oG8CwAoKKAbQGsIQAuKFYBASwDsBzAXSpvuYG58BfHvPgDGAGwCGKNPGTZ8hIXAYcArkOCIAFGGUAjEXSFRgEBAFt2tRkwA0UbXoNQkEAGYXOTAJTZefYeMkoABEDYFkCO3oANzFjKAB3RFQqaVQBeUUVNU1vXAjCYAALOhQAOkTkNEwsfjkoXwixJCQNCqQUpNy6wjoXKA0ikvKklCI20uMzFgx0TGUGZxdGCCQu-MIjYrK20fHJ02nMccX0jYylOBEIUpE4Jg0AA1SoAHIAEiw9k1NeF6hCiRQHQQCAMKBNZxIUoPTynQgNM4QEQoaB5M7nFCXa63e5PJKvD5fMy-KAlcEiBAQJogUlgorQJChOiKMQIEDQ2Hdep1BFQJgQYBBVwDb7uKxrM69fqDbYjIj7Q6zKDzRbLVbhdFQBQXK43O6PADCYgYDDgYSWCwSSSoH32vBhcI2lOAygQDC5vMISJRGvRztddK2w0q8u+LEdvN5zhEIrMYuYEo2vQ0AHkdAArCBqUpkEAoAZBnaeUqMUTKZz5-aeRNnaMC6Ay4OoUNTR0YrF63G2768f6A4GgpxI+tQh0erne1Fc9u6nGG42m82MJBW5A2rB2sfrSN+QR4bU0KAkOimITFDBQBgQeKwJIaABEx9PxXvtnvgB1VwC2tYANcaggBlxwBdofvTl90yMJGTUC8rxvEI1A0ED8AAekQowIEPCEoBQ6MkKw0JSghDQnzPOgQOQpw8IIojilI3Ds2jB8qLoYCBDIiDgFKejHxPYjmPwHDUMPflBVcfiDzCUwQEWC82NKIShRcBjuJfGitUyDs5wkxYELwZDABaGwAfceVMAkFiCBbBQQo4HiABBEQRFsBR5jCQAbWsAEDXAANVqBAGCa0ogA
